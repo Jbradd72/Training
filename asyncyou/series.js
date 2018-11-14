4f466f666d0ee6afa882fe7f02e8ce258c7aea99
Jbradd72 <bra16024@byui.edu>
2018-11-14 13:58:58 -0700

    Working through asyncyou

---
+++ asyncyou/series.js
@@ -0,0 +1,36 @@
+var http = require('http'), 
+    async = require('async');
+
+async.series({
+    requestOne: function(done){
+        var body = "";
+        http.get(process.argv[2].toString().trimRight(), function(res){
+            res.on('data', function(chunk){
+            body += chunk.toString();
+            });
+    
+            res.on('end', function(chunk){
+            done(null, body);
+            });
+        }).on('error', function(e){
+            done(e);
+        });
+    },
+    requestTwo: function(done){
+        var body = "";
+        http.get(process.argv[3].toString().trimRight(), function(res){
+            res.on('data', function(chunk){
+            body += chunk.toString();
+            });
+    
+            res.on('end', function(chunk){
+            done(null, body);
+            });
+        }).on('error', function(e){
+            done(e);
+        });
+    }
+  }, function done(err, bodies){
+    if (err) return console.error(err);
+    console.log(bodies);
+});
